f8e426f246ed909d83cd890899f8362c6e6910cd
Barnabas <davidbarnabas666@gmail.com>
2026-07-29 19:09:36 +0100

    Add ExplorePizza and NewestPizza components for pizza exploration and showcasing new arrivals

--- italian-pizza-restaurant/src/pages/Home.jsx
+++ italian-pizza-restaurant/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import AboutSection from "../components/home/AboutSection/AboutSection";
+import ExplorePizza from "../components/home/ExplorePizza/ExplorePizza";
 import Hero from "../components/home/Hero/Hero";
+import NewestPizza from "../components/home/NewestPizza/NewestPizza";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <AboutSection />
+      <NewestPizza />
+      <ExplorePizza />
     </>
   );
 };
